b9f1835bf7c47a045f5ee5082834e6956640cff0
Andrew Thomas <andrew@andrewrthomas.com>
2025-06-13 14:13:43 -0700

    nice and crappy

--- main.go
+++ main.go
@@ -22,30 +22,56 @@ func main() {
 			http.FileServer(http.Dir("static")),
 		),
 	)
-	http.HandleFunc("GET /", home)
-	http.HandleFunc("GET /{slug}", post)
+	http.HandleFunc("GET /{slug...}", post)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func home(respWriter http.ResponseWriter, req *http.Request) {
-	md := goldmark.New(goldmark.WithExtensions(meta.Meta))
-	posts := getPosts(md)
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.Execute(respWriter, TemplData{Posts: posts, Content: template.HTML("hello there")})
-}
-
 func post(respWriter http.ResponseWriter, req *http.Request) {
 	md := goldmark.New(goldmark.WithExtensions(meta.Meta))
-	posts := getPosts(md)
 
-	slug := req.PathValue("slug")
-	raw, err := os.ReadFile(fmt.Sprintf("posts/%s.md", slug))
+	dir, err := os.ReadDir("posts")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("%s\n", err)
 	}
 
 	ctx := parser.NewContext()
+	posts := make([]Post, len(dir)-1)
+	for i := range dir {
+		name := dir[i].Name()
+		if name == "welcome.md" {
+			continue
+		}
+		src, err := os.ReadFile(fmt.Sprintf("posts/%s", name))
+		if err != nil {
+			log.Fatal(err)
+		}
+		var buf bytes.Buffer
+		if err := md.Convert(src, &buf, parser.WithContext(ctx)); err != nil {
+			log.Fatal(err)
+		}
+		metaData := meta.Get(ctx)
+		title := metaData["title"].(string)
+		desc := metaData["description"].(string)
+		posts[i] = Post{
+			Title: title,
+			Desc:  desc,
+			Url:   fmt.Sprintf("%s", strings.TrimSuffix(dir[i].Name(), ".md")),
+		}
+	}
+
+	slug := req.PathValue("slug")
+	var raw []byte
+	if slug == "" {
+		raw, err = os.ReadFile(fmt.Sprintf("posts/welcome.md"))
+	} else {
+		raw, err = os.ReadFile(fmt.Sprintf("posts/%s.md", slug))
+	}
+	if err != nil {
+		respWriter.WriteHeader(http.StatusNotFound)
+		return
+	}
+
 	var buf bytes.Buffer
 	if err = md.Convert(raw, &buf, parser.WithContext(ctx)); err != nil {
 		log.Fatal(err)
@@ -65,35 +91,6 @@ func post(respWriter http.ResponseWriter, req *http.Request) {
 			Desc:    desc,
 		},
 	)
-}
-
-func getPosts(md goldmark.Markdown) []Post {
-	dir, err := os.ReadDir("posts")
-	if err != nil {
-		log.Fatalf("%s\n", err)
-	}
-	ctx := parser.NewContext()
-	posts := make([]Post, len(dir))
-	for i := range dir {
-		src, err := os.ReadFile(fmt.Sprintf("posts/%s", dir[i].Name()))
-		if err != nil {
-			log.Fatal(err)
-		}
-		var buf bytes.Buffer
-		if err := md.Convert(src, &buf, parser.WithContext(ctx)); err != nil {
-			log.Fatal(err)
-		}
-		metaData := meta.Get(ctx)
-		title := metaData["title"].(string)
-		desc := metaData["description"].(string)
-		posts[i] = Post{
-			Title: title,
-			Desc:  desc,
-			Url:   fmt.Sprintf("%s", strings.TrimSuffix(dir[i].Name(), ".md")),
-		}
-	}
-
-	return posts
 }
 
 type Post struct {
--- posts/welcome.md
+++ posts/welcome.md
@@ -3,4 +3,4 @@ title: Welcome!
 description: ""
 ---
 
-Hi, I'm Andrew.
+Hi, I'm Andrew. I like getting computers to do things, and sometimes, I like writing about that process. Things are a bit sparse here so far, mostly just some posts talking about a database I'm building, called "store", but stay tuned for more!
